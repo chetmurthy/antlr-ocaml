46261b09d4e9b4c7b64a440f1ac5f9322667cc60
Chet Murthy <chetsky@gmail.com>
2026-07-19 17:09:15 -0700

    meh

---
+++ tests/fixtures/pertest-files/LexerExec/CharSetWithQuote1/l.ml
@@ -0,0 +1,24 @@
+open Antlr
+open Exec
+
+let atns = Atns.load ~lexer_atn:"Lexer.interp" ~parser_atn:None ;;
+let atn = atns.Atns.lexer ;;
+
+let _A_action (self : R.recognizer_t) localCtx ruleIndex actionIndex =
+  output_string stdout "A\n" ;;
+
+let init ~input ~output =
+  let decisionToDFA : DFA.t array =
+    atn.Atn.decisionToState
+    |> Array.mapi (fun i stid ->
+           DFA.init atn Atn.LEXER stid i
+         ) in
+  let recog = R.init input ~output ~actions:[(0,_A_action)] () in
+  let interp : LAS.t =
+    Tracelog.with_disabled (fun () ->
+        LAS.init atn decisionToDFA [] ~recog ()
+      ) ()
+  in
+  Tracelog.with_disabled (fun () ->
+      Lexer.init ~recog ~interp ()
+    ) ()
