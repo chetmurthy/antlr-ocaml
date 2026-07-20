f6217ef061543c2770fad99e599dce86c1fd9da3
Chet Murthy <chetsky@gmail.com>
2026-07-20 09:50:38 -0700

    meh

---
+++ tests/fixtures/pertest-files/SemPredEvalLexer/EnumNotID/l.ml
@@ -0,0 +1,31 @@
+open Pa_ppx_base
+open Ppxutil
+open Antlr
+open Exec
+
+let atns = Atns.load ~lexer_atn:"Lexer.interp" ~parser_atn:None ;;
+let atn = atns.Atns.lexer ;;
+
+let _ENUM_sempred (self : R.recognizer_t) (cu : LASC.t) localCtx predIndex =
+  if predIndex = 0 then
+    (R.text self cu) = "enum"
+  else Fmt.(failwithf "ENUM_sempred: bad predIndex %d" predIndex)
+
+let actions = []
+let sempreds = [(0, _ENUM_sempred)]
+
+let init ~input ~output =
+  let decisionToDFA : DFA.t array =
+    atn.Atn.decisionToState
+    |> Array.mapi (fun i stid ->
+           DFA.init atn Atn.LEXER stid i
+         ) in
+  let recog = R.init input ~output ~actions ~sempreds () in
+  let interp : LAS.t =
+    Tracelog.with_disabled (fun () ->
+        LAS.init atn decisionToDFA [] ~recog ()
+      ) ()
+  in
+  Tracelog.with_disabled (fun () ->
+      Lexer.init ~recog ~interp ()
+    ) ()
---
+++ tests/fixtures/pertest-files/SemPredEvalLexer/LexerInputPositionSensitivePredicates/l.ml
@@ -0,0 +1,26 @@
+open Pa_ppx_base
+open Ppxutil
+open Antlr
+open Exec
+
+let atns = Atns.load ~lexer_atn:"Lexer.interp" ~parser_atn:None ;;
+let atn = atns.Atns.lexer ;;
+
+let actions = []
+let sempreds = []
+
+let init ~input ~output =
+  let decisionToDFA : DFA.t array =
+    atn.Atn.decisionToState
+    |> Array.mapi (fun i stid ->
+           DFA.init atn Atn.LEXER stid i
+         ) in
+  let recog = R.init input ~output ~actions ~sempreds () in
+  let interp : LAS.t =
+    Tracelog.with_disabled (fun () ->
+        LAS.init atn decisionToDFA [] ~recog ()
+      ) ()
+  in
+  Tracelog.with_disabled (fun () ->
+      Lexer.init ~recog ~interp ()
+    ) ()
