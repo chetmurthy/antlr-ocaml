1e5c1ab815aebffac2918a2a4a412122a9a4946e
Chet Murthy <chetsky@gmail.com>
2026-07-28 19:14:29 -0700

    meh

--- tests/fixtures/pertest-files/LexerExec/CharSet/l.ml
+++ tests/fixtures/pertest-files/LexerExec/CharSet/l.ml
@@ -8,5 +8,8 @@ let _I_action (self : R.recognizer_t) (cu : LASC.t) localCtx actionIndex =
   if actionIndex = 0 then
     output_string stdout "I\n" ;;
 
+let actions = [(0,_I_action)]
+let sempreds = []
+
 let init ~input ~output =
-  LexerBase.init ~atn ~actions:[(0,_I_action)] ~sempreds:[] ~input ~output
+  LexerBase.init ~atn ~actions ~sempreds ~input ~output
