5933d1b571be9396d139d6c7e5107fe60b9fd3b9
Chet Murthy <chetsky@gmail.com>
2026-08-03 14:18:35 -0700

    meh

--- tests/stringtemplate_test.ml
+++ tests/stringtemplate_test.ml
@@ -20,9 +20,40 @@ let test_parse_st ctxt =
   ; assert_equal () (ignore (Pa.Template.load ~file:"fixtures/antlrtest.7/Test.py"))
   ; assert_equal () (ignore (Pa.Template.load ~file:"fixtures/antlrtest.7/TestLexer.py"))
 
+let test_parse_file file ctxt =
+  let open Stringtemplate in
+  ()
+  ; assert_equal () (ignore (Pa.Template.load ~file))
+
+let test_parse_all_grammars () =
+  let g4list =
+    [Fpath.v "_generated"]
+    |> Bos.OS.Path.fold (fun a b -> a::b) []
+    |> Result.get_ok
+    |> List.filter (Fpath.has_ext "g4")
+  in
+ let tests =
+   g4list |>
+     List.map (fun f ->
+         let f = Fpath.to_string f in
+         f >:: (test_parse_file f))
+ in
+ tests
+
+let parse_fixed_files = 
+  "parse fixed files" >:::
+    ([
+        "fixtures/antlrtest.7/Makefile"
+      ; "fixtures/antlrtest.7/Test.py"
+      ; "fixtures/antlrtest.7/TestLexer.py"
+      ] |> List.map (fun f -> (f >:: test_parse_file f)))
 
 let suite = "Test Stringtemplate" >::: [
       "parse st"   >:: test_parse_st
+    ; "parse fixed files" >: parse_fixed_files
+(*
+    ; "parse grammars" >::: (test_parse_all_grammars())
+ *)
     ]
 
 let _ = 
