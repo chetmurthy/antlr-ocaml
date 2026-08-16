b556fd0e534d0bb2fd79657ee05c27abae389ffc
Chet Murthy <chetsky@gmail.com>
2026-08-15 19:54:10 -0700

    meh

---
+++ tests/fixtures/pertest-files/antlr4/STG2/ActionFuns_ST2.ml
@@ -0,0 +1,57 @@
+
+open Antlr
+open Exec
+
+let lDelim = Char.code '<'
+let rDelim = Char.code '>'
+
+let subtemplateDepth = ref 0
+
+let watch_subtemplateDepth (n : int) = ()
+
+let enterSubTemplate self cu =
+  incr subtemplateDepth ;
+  watch_subtemplateDepth !subtemplateDepth
+
+let exitSubTemplate self cu =
+  watch_subtemplateDepth !subtemplateDepth;
+  if !subtemplateDepth > 0 then begin
+      decr subtemplateDepth ;
+      ignore(R.popMode self : int)
+    end
+
+let subTemplateHasIDs self cu =
+  let rec idrec i =
+  let c = IS.la self.R._input i in
+  if c = Char.code '|' then true
+  else if c = Char.code '}' then false
+  else if c = C._EOF then false
+  else idrec (i+1)
+  in idrec 1
+
+let adjText self cu =
+  let c1 = IS.la self.R._input (-1) in
+  if c1 = Char.code '\\' then begin
+      let c2 = IS.la self._input 1 in
+      if c2 = Char.code '\\' then
+        IS.consume self._input
+      else if c2 = lDelim || c2 = Char.code '}' then
+                IS.consume self._input
+      else ()
+    end ;
+  true
+
+let isLDelimNotComment self cu =
+  IS.la self.R._input (-1) = lDelim && IS.la self.R._input 1 <> Char.code '!'
+
+let isRDelim self cu =
+  rDelim = IS.la self.R._input (-1)
+
+let isLTmplComment self cu =
+   IS.la self.R._input (-1) = lDelim && IS.la self._input (1) = Char.code '!'
+
+let isRTmplComment self cu =
+   IS.la self.R._input (-2) = Char.code '!' && IS.la self._input (-1) = rDelim
+
+let reset () =
+  subtemplateDepth := 0
