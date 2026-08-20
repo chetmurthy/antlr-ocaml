7a19ee7c5d7854d13c17a52946d3a0d3d432c964
Chet Murthy <chetsky@gmail.com>
2026-08-20 09:15:18 -0700

    meh

--- tests/st4eval_test.ml
+++ tests/st4eval_test.ml
@@ -1,8 +1,13 @@
 (**pp -syntax camlp5o -package pa_ppx_regexp,pa_ppx.deriving_plugins.std,pa_ppx.here *)
 
 open OUnit2
+
+open Pa_ppx_utils
+
 open Antlr
 open Antlrtest
+open Stringtemplate
+open Eval
 
 Pa_ppx_runtime.Exceptions.Ploc.pp_loc_verbose := true ;;
 
@@ -12,9 +17,49 @@ Exec.file_init ~dfast_cache:caches.dfast ~acs_cache:caches.acs ~ac_cache:caches.
 module STPa = Stringtemplate.Pa.STG2_STPa
 module STGPa = Stringtemplate.Pa.STG2_STGPa
 
+let stparse s =
+  let t =
+    s
+    |> STPa.Template.of_string
+    |> St_ops.coalesce
+    |> St_ops.insert_indentation
+  in
+  assert (St_ops.balanced_indentation t) ;
+  t
+
+let steval env t =
+  let open Doit in
+  let ctxt = Context.mk() in
+  (t
+   |> eval_elements ctxt env Indent.mt
+   |> render_attr_value) ()
+  |> FIW.render_stream
+  |> Std.list_of_stream
+  |> String.concat ""
+
 let test_eval_simple ctxt =
   let open Stringtemplate in
+  let printer x = Fmt.(str "%a" Dump.string x) in
   ()
+  ; assert_equal ~printer "Hello, FooBar!"
+      (steval [["name",MV [STRING "Foo"; STRING "Bar"]]]
+         (stparse {|<"Hello">, <name>!|}))
+  ; assert_equal ~printer {|
+  Hello, FooBar!
+|}
+      (steval [["name",MV [STRING "Foo"; STRING "Bar"]]]
+         (stparse {|
+  <"Hello">, <name>!
+|}))
+  ; assert_equal ~printer {|
+  Hello, Foo
+  Bar!
+|}
+      (steval [["name",MV [STRING "Foo"; STRING "Bar"]]]
+         (stparse {|
+  <"Hello">, <name; separator="\n">!
+|}))
+
 
 
 let suite = "Test St4 evaluation" >::: [
